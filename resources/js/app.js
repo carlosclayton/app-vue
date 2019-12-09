c92c5e1662e1848f9901fc8c9f363a4d8e09c2f6
Carlos Clayton <carlos.clayton@gmail.com>
2019-12-09 16:38:37 -0300

    Updating API URL

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,6 +30,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
 Vue.http.options.root = process.env.API_URL;
+
 require('./services/interceptor')
 
 new Vue({
